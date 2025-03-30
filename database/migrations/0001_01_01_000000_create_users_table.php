77cd6cbcbf96bbdb1418f5cbe8903acd9d856396
RyzkiHarmony <m.rizki00135@gmail.com>
2025-03-30 21:45:41 +0700

    ganti tipe data role menjadi enum atau bool

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration {
             $table->string('nama');
             $table->string('alamat');
             $table->string('no_hp');
-            $table->string('role');
+            $table->enum('role', ['dokter', 'pasien']);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
