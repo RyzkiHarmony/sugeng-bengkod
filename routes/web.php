fa140487e7ff3e38cdb07232848ab059e8072fee
RyzkiHarmony <m.rizki00135@gmail.com>
2025-04-18 11:21:16 +0700

    rapihin routing

--- routes/web.php
+++ routes/web.php
@@ -6,37 +6,56 @@ use App\Http\Controllers\ObatController;
 use App\Http\Controllers\PeriksaController;
 use Illuminate\Support\Facades\Route;
 
+// Landing page
 Route::get('/', function () {
     return view('layout.landingpage');
 });
 
+// Auth routes
+Route::prefix('auth')->group(function () {
+    Route::get('/register', [AuthController::class, "registerPage"]);
+    Route::get('/login', [AuthController::class, "loginPage"]);
+    Route::post('/register', [AuthController::class, "register"]);
+    Route::post('/login', [AuthController::class, "login"]);
+    Route::post('/logout', [AuthController::class, "logout"])->middleware('auth');
+});
 
-Route::get('/auth/register', [AuthController::class, "registerPage"]);
-Route::get('/auth/login', [AuthController::class, "loginPage"]);
-Route::post('/auth/register', [AuthController::class, "register"]);
-Route::post('/auth/login', [AuthController::class, "login"]);
-Route::post('/auth/logout', [AuthController::class, "logout"])->middleware('auth');
+// Dokter routes
+Route::prefix('dokter')->middleware('role:dokter')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dokter.index');
+    })->name('dokter.dashboard');
 
+    // Obat routes
+    Route::prefix('obat')->group(function () {
+        Route::get('/', [ObatController::class, "index"]);
+        Route::get('/create', [ObatController::class, "create"]);
+        Route::post('/', [ObatController::class, "store"]);
+        Route::get('/{id}/edit', [ObatController::class, "edit"]);
+        Route::put('/{id}', [ObatController::class, "update"]);
+        Route::delete('/{id}', [ObatController::class, "destroy"]);
+    });
 
-Route::get('/dokter/dashboard', function () {
-    return view('dokter.index');
-})->name('dokter.dashboard')->middleware('role:dokter');
-Route::get('/dokter/obat', [ObatController::class, "index"])->middleware('role:dokter');
-Route::get('/dokter/obat/create', [ObatController::class, "create"])->middleware('role:dokter');
-Route::post('/dokter/obat', [ObatController::class, "store"])->middleware('role:dokter');
-Route::get('/dokter/obat/{id}/edit', [ObatController::class, "edit"])->middleware('role:dokter');
-Route::put('/dokter/obat/{id}', [ObatController::class, "update"])->middleware('role:dokter');
-Route::delete('/dokter/obat/{id}', [ObatController::class, "destroy"])->middleware('role:dokter');
-Route::get('/dokter/memeriksa', [MemeriksaController::class, "index"])->middleware('role:dokter');
-Route::get('/dokter/memeriksa/{id}', [MemeriksaController::class, "memeriksa"])->middleware('role:dokter');
-Route::post('/dokter/memeriksa', [MemeriksaController::class, "store"])->middleware('role:dokter');
-Route::get('/dokter/memeriksa/{id}/edit', [MemeriksaController::class, "edit"])->middleware('role:dokter');
-Route::put('/dokter/memeriksa/{id}', [MemeriksaController::class, "update"])->middleware('role:dokter');
+    // Memeriksa routes
+    Route::prefix('memeriksa')->group(function () {
+        Route::get('/', [MemeriksaController::class, "index"]);
+        Route::get('/{id}', [MemeriksaController::class, "memeriksa"]);
+        Route::post('/', [MemeriksaController::class, "store"]);
+        Route::get('/{id}/edit', [MemeriksaController::class, "edit"]);
+        Route::put('/{id}', [MemeriksaController::class, "update"]);
+    });
+});
 
+// Pasien routes
+Route::prefix('pasien')->middleware('role:pasien')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('pasien.index');
+    })->name('pasien.dashboard');
 
-Route::get('/pasien/dashboard', function () {
-    return view('pasien.index');
-})->name('pasien.dashboard')->middleware('role:pasien');
-Route::get('/pasien/periksa', [PeriksaController::class, "index"])->middleware('role:pasien');
-Route::get('/pasien/periksa/create', [PeriksaController::class, "create"])->middleware('role:pasien');
-Route::post('/pasien/periksa', [PeriksaController::class, "store"])->middleware('role:pasien');
+    // Periksa routes
+    Route::prefix('periksa')->group(function () {
+        Route::get('/', [PeriksaController::class, "index"]);
+        Route::get('/create', [PeriksaController::class, "create"]);
+        Route::post('/', [PeriksaController::class, "store"]);
+    });
+});
